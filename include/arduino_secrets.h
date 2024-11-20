26518f35917ef84f81b2e84b45267cbbbcd20a9c
Your Name <you@example.com>
2024-11-20 08:30:33 -0500

    Add arduino_secrets.h with network and API credentials

---
+++ include/arduino_secrets.h
@@ -0,0 +1,7 @@
+#define EAP_IDENTITY "abc@cmaisonneuve.qc.ca"
+  #define EAP_USERNAME "abc@cmaisonneuve.qc.ca"
+  #define EAP_PASSWORD "123456"
+  #define SECRET_SSID "eduroam"
+
+  #define API_KEY "123456"
+  #define API_HOST "api.simplyprint.io/123456/endpoint/"
